7e6f8aebb38a11361e833c21825f12da9688da30
mysolution20 <mysolution@op.pl>
2020-05-03 11:58:38 +0200

    Issue with underlined imageback.

--- src/main/java/com/kodilla/Blackjack.java
+++ src/main/java/com/kodilla/Blackjack.java
@@ -1,30 +1,36 @@
 package com.kodilla;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 
 public class Blackjack extends Application {
 
+     Image imageback =
+            new Image("file:resources/table.png");
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 300, Color.BLACK);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
 
-        Rectangle r = new Rectangle(25, 25, 250, 250);
-        r.setFill(Color.BLUE);
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
 
-        root.getChildren().add(r);
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
         primaryStage.setTitle("BlackJack");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 }
